b8c7e7800fb0a535d4cb62ee24636b7f00df4621
TakashiSato <takashisato.235711@gmail.com>
2015-11-03 17:36:37 +0900

    refs ログイン判定のコメント追記

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
+# 戻り値が true ならサインイン済み。
+# => user_signed_in?
+# サインインしている全ユーザー取得
+# => current_user
+# ユーザーのセッション情報
+# => user_session
+
   def index
   end
 end
