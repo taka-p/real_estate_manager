3eacf715e0a33e97e881c6dca71e06c002d931cf
TakashiSato <takashisato.235711@gmail.com>
2015-11-03 17:29:31 +0900

    refs homeのview,controllerを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+
+  root 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
